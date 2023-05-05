abd00c5286e13585aae25d89d97e3a241a30548f
saku-1101 <sakupi1101@outlook.jp>
2023-05-06 09:13:40 +1000

    [feat]cocoaPods installation and MVC is structured

---
+++ ChatApp/Controllers/VideoPlayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayViewController.swift
+//  ChatApp
+//
+//  Created by 安達さくら on 2023/05/06.
+//
+
+import UIKit
+
+class VideoPlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
