f23c64df318d5dbb85f464bd1c75ffde63730f44
saku-1101 <sakupi1101@outlook.jp>
2023-05-06 15:58:53 +1000

    [add]choose picture from the photo library function done

--- ChatApp/Controllers/Login/RegisterViewController.swift
+++ ChatApp/Controllers/Login/RegisterViewController.swift
@@ -21,6 +21,9 @@ class RegisterViewController: UIViewController {
         imageView.image = UIImage(systemName: "person")
         imageView.tintColor = .gray
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
         return imageView
     }()
     
@@ -127,6 +130,7 @@ class RegisterViewController: UIViewController {
     
     @objc private func didTapChangeProfilePic() {
         print("change pic")
+        presentPhotoActionSheet()
     }
     
     override func viewDidLayoutSubviews() {
@@ -135,6 +139,7 @@ class RegisterViewController: UIViewController {
         
         let size = scrollView.width/3
         imageView.frame = CGRect(x: (view.width-size)/2, y: 20, width: size, height: size)
+        imageView.layer.cornerRadius = imageView.width/2.0
         
         firstNameField.frame = CGRect(x: 30, y: imageView.bottom+10, width: scrollView.width-60, height: 52)
         
@@ -157,14 +162,14 @@ class RegisterViewController: UIViewController {
         
         // ensure the textFields are not empty and password is over 6 letters long
         guard let firstName = firstNameField.text, let lastName = lastNameField.text, let email = emailField.text, let password = passwordField.text, !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty, !password.isEmpty, password.count >= 6 else{
-            alertUseLoginError()
+            alertUserLoginError()
             return
         }
         
         // firebase login
     }
     
-    func alertUseLoginError() {
+    func alertUserLoginError() {
         let alert = UIAlertController(title: "Hang on!", message: "Please enter all the information to create a new account.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dissmiss", style: .cancel, handler: nil))
         present(alert, animated: true)
@@ -186,5 +191,52 @@ extension RegisterViewController: UITextFieldDelegate {
             registerButtonTapped()
         }
         return true
+    }
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // get the results from camera roll
+    
+    // this is called from didTapChangeProfilePic
+    func presentPhotoActionSheet() {
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like set your Profile picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in self?.presentCamera()})) // with memory retention
+        actionSheet.addAction(UIAlertAction(title: "Chose Photo", style: .default, handler: { [weak self] _ in self?.presentPhotoPicker()}))
+        present(actionSheet, animated: true)
+    }
+    
+    // need to create the functions respectivelly called Cancel and Take photo
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker()
+    {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    // when user takes a photo or select a photo
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // grab the image from inside the dictionary
+        picker.dismiss(animated: true, completion: nil)
+        print(info)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else{
+            return
+        }// grab the cropped image
+        self.imageView.image = selectedImage // update imageView to the seleted image
+    }
+    
+    // if the user cancelled taking a photo or picture
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
