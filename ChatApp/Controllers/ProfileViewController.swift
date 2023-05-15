6e0b07dba178c2ca228c25f8ace7ff1636b8dc3b
saku-1101 <sakupi1101@outlook.jp>
2023-05-15 15:03:23 +1000

    [add]Log out with Cancel Option

--- ChatApp/Controllers/ProfileViewController.swift
+++ ChatApp/Controllers/ProfileViewController.swift
@@ -12,7 +12,7 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet var tableview: UITableView!
     
-    let data = ["Lob Out"]
+    let data = ["Log Out"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,14 +41,26 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        do {
-            try FirebaseAuth.Auth.auth().signOut()
-            let vc = LoginViewController()
-            let nav = UINavigationController(rootViewController: vc)
-            nav.modalPresentationStyle = .fullScreen
-            present(nav, animated: true)
-        } catch {
-            print("Failed to Log Out")
-        }
+        let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { [weak self] _ in
+            
+            guard let strongSelf = self else {
+                return
+            }
+            
+            do {
+                try FirebaseAuth.Auth.auth().signOut()
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.modalPresentationStyle = .fullScreen
+                strongSelf.present(nav, animated: true)
+            } catch {
+                print("Failed to Log Out")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(actionSheet, animated: true)
+    
     }
 }
