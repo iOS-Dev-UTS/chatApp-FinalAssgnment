473abc61dd73d8cfec0f468d4daf25afb68fd2e7
Bailey Gee <bailey.gee@student.uts.edu.au>
2023-05-22 00:19:15 +1000

    [add] completed search and local user array

--- ChatApp/Controllers/NewConversationViewController.swift
+++ ChatApp/Controllers/NewConversationViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class NewConversationViewController: UIViewController {
     
+   
     private var users = [[String: String]]()
+    private var results = [[String: String]]()
     private var hasFetched = false
     
     private let searchBar: UISearchBar = {
@@ -38,17 +40,49 @@ class NewConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.addSubview(noResultsLabel)
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        view.backgroundColor = .systemBackground
         navigationController?.navigationBar.topItem?.titleView = searchBar
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(dismissSelf))
-        searchBar.becomeFirstResponder()
+        //searchBar.becomeFirstResponder()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        noResultsLabel.frame = CGRect(x: view.width/4,
+                                      y: (view.height-200)/2,
+                                      width: view.width/2,
+                                      height: 200)
     }
     
     @objc private func dismissSelf() {
-        dismiss(animated: true)
+            dismiss(animated: true)
+        }
+    
+}
+extension NewConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
@@ -58,17 +92,22 @@ extension NewConversationViewController: UISearchBarDelegate {
         guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
             return
         }
+        searchBar.resignFirstResponder()
+        results.removeAll()
         self.searchUsers(query: text)
     }
     
     func searchUsers(query: String) {
         if hasFetched {
+            filterUsers(with: query)
             
         } else {
             DatabaseManager.shared.getAllUsers(completion: { [weak self] result in
                 switch result {
                 case .success(let usersCollection):
+                    self?.hasFetched = true
                     self?.users = usersCollection
+                    self?.filterUsers(with: query)
                 case .failure:
                     print("Failed to get users")
                 }
@@ -76,6 +115,33 @@ extension NewConversationViewController: UISearchBarDelegate {
         }
     }
 
+    func filterUsers(with term: String) {
+        guard hasFetched else {
+            return
+        }
+        
+        let results: [[String: String]] = self.users.filter({
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            return name.hasPrefix(term.lowercased())
+        })
+        
+        self.results = results
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        if results.isEmpty {
+            noResultsLabel.isHidden = false
+            tableView.isHidden = true
+        } else {
+            noResultsLabel.isHidden = true
+            tableView.isHidden = false
+            tableView.reloadData()
+        }
+    }
     
     
 }
