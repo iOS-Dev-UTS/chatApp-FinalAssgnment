e46974913332194e4a8296ef5333bedc9d9179aa
Bailey Gee <bailey.gee@student.uts.edu.au>
2023-05-26 13:54:26 +1000

    [add] Start new conversation with searched user

--- ChatApp/Controllers/ChatViewController.swift
+++ ChatApp/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MessageKit
+import InputBarAccessoryView
 
 struct Message: MessageType {
     var sender: MessageKit.SenderType
@@ -32,24 +33,62 @@ struct Sender: SenderType {
 
 class ChatViewController: MessagesViewController {
     
+    public var isNewConversation = false
+    public let otherUserEmail: String
+    
     private var messages = [Message]()
     
     private let selfSender = Sender(photoURL: "", senderId: "1", displayName: "John Smith")
+    
+    init(with email: String) {
+        self.otherUserEmail = email
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("Hello World")))
-        
-        messages.append(Message(sender: selfSender, messageId: "2", sentDate: Date(), kind: .text("Hello World Hello World Hello World Hello World")))
-        
+//        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("Hello World")))
+//        
+//        messages.append(Message(sender: selfSender, messageId: "2", sentDate: Date(), kind: .text("Hello World Hello World Hello World Hello World")))
+//
         view.backgroundColor = .red
         
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        messageInputBar.inputTextView.becomeFirstResponder()
+    }
+    
+}
+extension ChatViewController: InputBarAccessoryViewDelegate {
+    
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        guard !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
+        print("Sending: \(text)")
+        
+        if isNewConversation {
+            
+        } else {
+            
+        }
+        
+    }
+    
 }
 
 extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
