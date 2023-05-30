7a282d5732b06298be8af70893f0413d514ca2b5
saku-1101 <sakupi1101@outlook.jp>
2023-05-30 16:55:38 +0900

    [fix]fetch the conversation from the db and displayed them on the screen in realtime

--- ChatApp/Controllers/ChatViewController.swift
+++ ChatApp/Controllers/ChatViewController.swift
@@ -60,8 +60,8 @@ class ChatViewController: MessagesViewController {
     }()
     
     public var isNewConversation = false
-    public let conversationId: String?
-    public let otherUserEmail: String
+    public var conversationId: String?
+    public var otherUserEmail: String
     
     private var messages = [Message]()
     
@@ -70,17 +70,14 @@ class ChatViewController: MessagesViewController {
             print("sender error")
             return nil
         }
-        return Sender(photoURL: "", senderId: email, displayName: "John Smith")
+        let safeEmail = DatabaseManager.validEmail(emailAddress: email)
+        return Sender(photoURL: "", senderId: safeEmail, displayName: "Me")
     }
     
     init(with email: String, id: String?) {
         self.conversationId = id
         self.otherUserEmail = email
         super.init(nibName: nil, bundle: nil)
-        print("conversationId while init \(conversationId ?? "no conversationId")")
-        if let conversationId = conversationId {
-            listenForMessages(id: conversationId)
-        }
     }
     
     required init?(coder: NSCoder) {
@@ -93,10 +90,7 @@ class ChatViewController: MessagesViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .red
-        
-        if let conversationId = conversationId {
-            listenForMessages(id: conversationId)
-        }
+    
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
@@ -104,104 +98,134 @@ class ChatViewController: MessagesViewController {
         
     }
     
-    private func listenForMessages(id: String) {
+    private func listenForMessages(id: String, shouldScrollToBottom: Bool) {
         DatabaseManager.shared.getAllMessagesForConversation(with: id, completion: { [weak self] result in
             switch result {
             case .success(let messages):
+                print("success in getting messages: \(messages)")
                 guard !messages.isEmpty else {
-                    print("conversationId in listenForMessages\(id)")
-                    print("messages is empty")
+                    print("messages are empty")
                     return
                 }
-                
-                print("Messages in listenForMessages: \(messages)")
-                
                 self?.messages = messages
-                print("self.messages in listenForMessages: \(self?.messages)")
-                
-                DispatchQueue.main.async{
+
+                DispatchQueue.main.async {
                     self?.messagesCollectionView.reloadDataAndKeepOffset()
+
+                    if shouldScrollToBottom {
+                        self?.messagesCollectionView.scrollToLastItem()
+                    }
                 }
-                
-                print("successfully listen for messages")
-                
             case .failure(let error):
-                print ("failed to get messages: \(error)")
-                
+                print("failed to get messages: \(error)")
             }
         })
     }
 
+
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        messageInputBar.inputTextView.becomeFirstResponder()
-    }
-    
+           super.viewDidAppear(animated)
+           messageInputBar.inputTextView.becomeFirstResponder()
+           if let conversationId = conversationId {
+               listenForMessages(id: conversationId, shouldScrollToBottom: true)
+           }
+       }
 }
+
 extension ChatViewController: InputBarAccessoryViewDelegate {
-    
+
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
         guard !text.replacingOccurrences(of: " ", with: "").isEmpty,
-              let selfSender = self.selfSender,
-              let messageId = createMessageId()
-        else {
-            print("send error")
-            return
+            let selfSender = self.selfSender,
+            let messageId = createMessageId() else {
+                return
         }
-        
+
         print("Sending: \(text)")
-        
+
+        let message = Message(sender: selfSender,
+                               messageId: messageId,
+                               sentDate: Date(),
+                               kind: .text(text))
+
+        // Send Message
         if isNewConversation {
-            let message = Message(sender: selfSender,
-                                  messageId: messageId,
-                                  sentDate: Date(),
-                                  kind: .text(text))
-            
-            DatabaseManager.shared.createNewConversation(with: otherUserEmail, name: self.title ?? "User", firstMessage: message, completion: { success in
+            // create convo in database
+            DatabaseManager.shared.createNewConversation(with: otherUserEmail, name: self.title ?? "User", firstMessage: message, completion: { [weak self]success in
                 if success {
                     print("message sent")
-                } else {
+                    self?.isNewConversation = false
+                    let newConversationId = "conversation_\(message.messageId)"
+                    self?.conversationId = newConversationId
+                    self?.listenForMessages(id: newConversationId, shouldScrollToBottom: true)
+                    self?.messageInputBar.inputTextView.text = nil
+                }
+                else {
+                    print("faield ot send")
+                }
+            })
+        }
+        else {
+            guard let conversationId = conversationId, let name = self.title else {
+                return
+            }
+
+            // append to existing conversation data
+            DatabaseManager.shared.sendMessage(to: conversationId, otherUserEmail: otherUserEmail, name: name, newMessage: message, completion: { [weak self] success in
+                if success {
+                    self?.messageInputBar.inputTextView.text = nil
+                    print("message sent")
+                }
+                else {
                     print("failed to send")
                 }
             })
-        } else {
-            
         }
-        
     }
-    
+
     private func createMessageId() -> String? {
+        // date, otherUesrEmail, senderEmail, randomInt
         guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String else {
-            print("id error")
             return nil
         }
-        
-        let validEmail = DatabaseManager.validEmail(emailAddress: currentUserEmail)
-        
-        
+
+        let safeCurrentEmail = DatabaseManager.validEmail(emailAddress: currentUserEmail)
+
         let dateString = Self.dateFormatter.string(from: Date())
-        let newId = "\(otherUserEmail)_\(validEmail)_\(dateString)"
-        print("created message id: \(newId)")
-        return newId
+        let newIdentifier = "\(otherUserEmail)_\(safeCurrentEmail)_\(dateString)"
+
+        print("created message id: \(newIdentifier)")
+
+        return newIdentifier
     }
-    
+
 }
 
 extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
-    func currentSender() -> MessageKit.SenderType {
+    func currentSender() -> SenderType {
         if let sender = selfSender {
             return sender
         }
+
         fatalError("Self sender is nil")
     }
-    
-    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
         return messages[indexPath.section]
     }
-    
-    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
         return messages.count
     }
-    
-    
+
+    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        let sender = message.sender
+        if sender.senderId == selfSender?.senderId {
+            // our message that we've sent
+            return .link
+        }
+
+        return .secondarySystemBackground
+    }
+
 }
